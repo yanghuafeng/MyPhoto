f9161a42d871320b8549526425200441e2076617
yanghuafeng <1207378804@qq.com>
2019-12-13 15:21:59 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myphoto/MainActivity.java
@@ -0,0 +1,185 @@
+package com.example.myphoto;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import me.iwf.photopicker.PhotoPickerActivity;
+import me.iwf.photopicker.utils.PhotoPickerIntent;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    private TextView textView;
+    private String path = Environment.getExternalStorageDirectory()+"/myphoto";
+    private String pngurl = "file:///"+path+"/index.html";
+    private String jpgurl = "file:///"+path+"/index.html";
+    private static final String[] authBaseArr = {//申请权限类型
+            Manifest.permission.MOUNT_UNMOUNT_FILESYSTEMS,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+    private static final int authBaseRequestCode = 1;
+    private ArrayList<String> photos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView)findViewById(R.id.web_view);
+        textView = (TextView)findViewById(R.id.text);
+        webView.setWebViewClient(new WebViewClient());
+        initNavi();
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("myphoto",MODE_PRIVATE);
+        if(sharedPreferences.getBoolean("FIRST_START", true)) {
+            try {
+                addText("正在解压文件。。。");
+                Utils.unZipAssets(this, "package.zip", path, true);
+                sharedPreferences.edit().putBoolean("FIRST_START", false).apply();
+                addText("解压完成，右上角选择图片");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else {
+            addText("右上角选择图片");
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menu.add(1,1,1,"选择图片");
+        menu.add(1,2,2,"显示图片");
+        menu.add(1,3,3,"显示日志");
+        menu.add(1,4,4,"清空日志");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        switch (id){
+            case 1:
+                PhotoPickerIntent intent2 = new PhotoPickerIntent(MainActivity.this);
+                intent2.setPhotoCount(12);
+                startActivityForResult(intent2, authBaseRequestCode);
+                break;
+            case 2:
+                textView.setVisibility(View.GONE);
+                webView.loadUrl(jpgurl);
+                webView.setVisibility(View.VISIBLE);
+                break;
+            case 3:
+                textView.setVisibility(View.VISIBLE);
+                webView.setVisibility(View.GONE);
+                break;
+            case 4:
+                textView.setText("");
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == authBaseRequestCode) {
+            if (data != null) {
+                photos = data.getStringArrayListExtra(PhotoPickerActivity.KEY_SELECTED_PHOTOS);
+                if (photos != null && photos.size() == 12) {
+
+                    addText("选择图片完成");
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            savePhoto(photos);
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    textView.setVisibility(View.GONE);
+                                    webView.loadUrl(jpgurl);
+
+                                }
+                            });
+                        }
+                    }).start();
+                }else{
+                    Toast.makeText(this,"请选择12张",Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    private void savePhoto(ArrayList<String> photos){
+        int i=0;
+        for(String mpath:photos){
+            i++;
+            Bitmap bitmap = Utils.getImage(mpath,i<=6);
+            Utils.savePhoto(bitmap,path+"/img/",+i+".jpg");
+            final  String name = "保存文件："+path+"/img/"+i+".jpg";
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    addText(name);
+                }
+            });
+
+        }
+    }
+
+    private boolean hasBasePhoneAuth() {
+        PackageManager pm = getPackageManager();
+        for (String auth : authBaseArr) {
+            if (pm.checkPermission(auth, getPackageName()) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void initNavi() {
+        // 申请权限
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!hasBasePhoneAuth()) {
+                this.requestPermissions(authBaseArr, authBaseRequestCode);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void addText(String content) {
+        textView.append(content);
+        textView.append("\n");
+        int offset = textView.getLineCount() * textView.getLineHeight();
+        if (offset > textView.getHeight()) {
+            textView.scrollTo(0, offset - textView.getHeight());
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myphoto/Utils.java
@@ -0,0 +1,133 @@
+package com.example.myphoto;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+/**
+ * Created by Administrator at 14:15 on 2019-12-12.
+ */
+
+public class Utils {
+    public static void unZipAssets(Context context, String assetName, String outputDirectory, boolean isReWrite) throws IOException {
+        // 创建解压目标目录
+        File file = new File(outputDirectory);
+        // 如果目标目录不存在，则创建
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+
+        // 打开压缩文件
+        InputStream inputStream = context.getAssets().open(assetName);
+        ZipInputStream zipInputStream = new ZipInputStream(inputStream);
+        // 读取一个进入点
+        ZipEntry zipEntry = zipInputStream.getNextEntry();
+        // 使用1Mbuffer
+        byte[] buffer = new byte[1024 * 1024];
+        // 解压时字节计数
+        int count = 0;
+        // 如果进入点为空说明已经遍历完所有压缩包中文件和目录
+        while (zipEntry != null) {
+            // 如果是一个目录
+            if (zipEntry.isDirectory()) {
+                file = new File(outputDirectory + File.separator + zipEntry.getName());
+                // 文件需要覆盖或者是文件不存在
+                if (isReWrite || !file.exists()) {
+                    file.mkdirs();
+                }
+            } else {
+                // 如果是文件
+                file = new File(outputDirectory + File.separator + zipEntry.getName());
+                // 文件需要覆盖或者文件不存在，则解压文件
+                if (isReWrite || !file.exists()) {
+                    file.createNewFile();
+                    FileOutputStream fileOutputStream = new FileOutputStream(file);
+                    while ((count = zipInputStream.read(buffer)) > 0) {
+                        fileOutputStream.write(buffer, 0, count);
+                    }
+                    fileOutputStream.close();
+                }
+            }
+            // 定位到下一个文件入口
+            zipEntry = zipInputStream.getNextEntry();
+        }
+        zipInputStream.close();
+    }
+
+    //对bitmap进行质量压缩
+    private static Bitmap compressImage(Bitmap image) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        image.compress(Bitmap.CompressFormat.JPEG, 100, baos);//质量压缩方法，这里100表示不压缩，把压缩后的数据存放到baos中
+        int options = 100;
+        while (baos.toByteArray().length / 1024 > 500) {  //循环判断如果压缩后图片是否大于100kb,大于继续压缩
+            baos.reset();//重置baos即清空baos
+            image.compress(Bitmap.CompressFormat.JPEG, options, baos);//这里压缩options%，把压缩后的数据存放到baos中
+            options -= 10;//每次都减少10
+        }
+        ByteArrayInputStream isBm = new ByteArrayInputStream(baos.toByteArray());//把压缩后的数据baos存放到ByteArrayInputStream中
+        return BitmapFactory.decodeStream(isBm, null, null);//把ByteArrayInputStream数据生成图片
+
+    }
+    //传入图片路径，返回压缩后的bitmap
+    public static Bitmap getImage(String srcPath,Boolean small) {
+        if (TextUtils.isEmpty(srcPath)) //如果图片路径为空 直接返回
+            return null;
+        BitmapFactory.Options newOpts = new BitmapFactory.Options();
+        //开始读入图片，此时把options.inJustDecodeBounds 设回true了
+        newOpts.inJustDecodeBounds = true;
+        Bitmap bitmap = BitmapFactory.decodeFile(srcPath, newOpts);//此时返回bm为空
+        newOpts.inJustDecodeBounds = false;
+        int w = newOpts.outWidth;
+        int h = newOpts.outHeight;
+        //现在主流手机比较多是800*480分辨率，所以高和宽我们设置为
+        float hh = 400f;//这里设置高度为800f
+        float ww = 400f;//这里设置宽度为480f
+        if(small) {
+            hh = 100f;//这里设置高度为800f
+            ww = 100f;//这里设置宽度为480f
+        }
+        //缩放比。由于是固定比例缩放，只用高或者宽其中一个数据进行计算即可
+        int be = 1;//be=1表示不缩放
+        if (w > h && w > ww) {//如果宽度大的话根据宽度固定大小缩放
+            be = (int) (newOpts.outWidth / ww);
+        } else if (w < h && h > hh) {//如果高度高的话根据宽度固定大小缩放
+            be = (int) (newOpts.outHeight / hh);
+        }
+        if (be <= 0)
+            be = 1;
+        newOpts.inSampleSize = be;//设置缩放比例
+        //重新读入图片，注意此时已经把options.inJustDecodeBounds 设回false了
+        bitmap = BitmapFactory.decodeFile(srcPath, newOpts);
+        return compressImage(bitmap);//压缩好比例大小后再进行质量压缩
+    }
+
+    public static void savePhoto(Bitmap bitmap, String path, String name) {
+        try {
+            File dirFile = new File(path);
+            if (!dirFile.exists()) {              //如果不存在，那就建立这个文件夹
+                dirFile.mkdirs();
+            }
+            File file = new File(path, name);
+            FileOutputStream fos = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+            fos.flush();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
